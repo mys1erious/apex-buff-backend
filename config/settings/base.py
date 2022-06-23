31b643dd664ee94d74e9176738966110823553da
mys1erious <subtletyzeros@gmail.com>
2022-06-23 14:22:49 +0200

    Static, media and dev_heroku settings setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ sys.path.append(os.path.join(BASE_DIR, 'apex_buff'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
---
+++ config/settings/dev_heroku.py
@@ -0,0 +1,35 @@
+from .base import *
+
+
+from .base import *
+
+
+DEBUG = True
+
+
+INSTALLED_APPS += ['debug_toolbar']
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'apex_buff', 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'apex_buff', 'media')
+
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
