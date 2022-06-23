74d2e52bb4b18db80cddcc68a088dfd926e605cf
mys1erious <subtletyzeros@gmail.com>
2022-06-23 16:43:14 +0200

    Added Schema and Swagger-UI documentation for API

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.permissions import AllowAny
+
+from rest_framework.schemas import get_schema_view
 
 from core import apiv1_urls
 
@@ -10,6 +15,28 @@ urlpatterns = [
         name='api'
     ),
 
+    path(
+        route='',
+        view=lambda request: redirect('documentation')
+    ),
+    path(
+        route='documentation/',
+        view=TemplateView.as_view(
+            template_name='swagger-ui.html',
+            extra_context={'schema_url': 'schema'},
+        ),
+        name='documentation'
+    ),
+    path(
+        route='schema/',
+        view=get_schema_view(
+            title='Schema for Apex Buff',
+            description='Schema for API of Apex Legends (not official)',
+            version='1.0.0',
+            permission_classes=(AllowAny, )
+        ),
+        name='schema'
+    ),
     path(
         route='__debug__/',
         view=include('debug_toolbar.urls')
